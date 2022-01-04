df9de23e59d3ebd625467b89160f15b176c324ec
VinceBaylon <vincentbaylon@gmail.com>
2022-01-04 09:04:32 -0800

    Changed recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,20 +1,17 @@
 class RecipesController < ApplicationController
   require 'rest-client'
 
-  before_action :authorize, except: [:get_recipes, :more_recipes]
+  before_action :authorize, except: [:get_recipes]
 
   def get_recipes
-    # Rails.cache.fetch([self, :recipes], expires_in: 12.hours) do
+    response = Rails.cache.fetch([self, :recipes], expires_in: 12.hours) do
       id = ENV["APP_ID"]
       key = ENV["APP_KEY"]
       url = "https://api.edamam.com/api/recipes/v2?type=public&q=vegan&app_id=#{id}&app_key=#{key}&health=vegan"
       response = RestClient.get(url)
-      render json: response, status: :ok
-    # end
+    end
+
+    render json: response, status: :ok
   end
 
-  private
-  def recipe_params
-    params.permit(:url)
-  end
 end
