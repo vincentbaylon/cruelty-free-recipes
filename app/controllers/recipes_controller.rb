903d71b6e706b4d14dd8d3e966d137970766003c
VinceBaylon <vincentbaylon@gmail.com>
2022-01-04 10:15:45 -0800

    More fetch cache testing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipesController < ApplicationController
   before_action :authorize, except: [:get_recipes]
 
   def get_recipes
-    response = Rails.cache.fetch([self, :recipes], expires_in: 12.hours) do
+    response = Rails.cache.fetch(:recipes, expires_in: 12.hours) do
       id = ENV["APP_ID"]
       key = ENV["APP_KEY"]
       url = "https://api.edamam.com/api/recipes/v2?type=public&q=vegan&app_id=#{id}&app_key=#{key}&health=vegan"
