8f0662fb7d054b3e2e42b19ff8730c3d27e54397
VinceBaylon <vincentbaylon@gmail.com>
2022-01-03 20:50:24 -0800

    Removed fetch cache

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,13 +4,13 @@ class RecipesController < ApplicationController
   before_action :authorize, except: [:get_recipes, :more_recipes]
 
   def get_recipes
-    Rails.cache.fetch([self, :recipes], expires_in: 12.hours) do
+    # Rails.cache.fetch([self, :recipes], expires_in: 12.hours) do
       id = ENV["APP_ID"]
       key = ENV["APP_KEY"]
       url = "https://api.edamam.com/api/recipes/v2?type=public&q=vegan&app_id=#{id}&app_key=#{key}&health=vegan"
       response = RestClient.get(url)
       render json: response, status: :ok
-    end
+    # end
   end
 
   private
