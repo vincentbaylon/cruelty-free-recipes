b24f3d657a82f55cdc95b5430ad17acecead2fa8
VinceBaylon <vincentbaylon@gmail.com>
2022-01-04 09:48:21 -0800

    More fetch cache testing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,7 +8,7 @@ class RecipesController < ApplicationController
       id = ENV["APP_ID"]
       key = ENV["APP_KEY"]
       url = "https://api.edamam.com/api/recipes/v2?type=public&q=vegan&app_id=#{id}&app_key=#{key}&health=vegan"
-      RestClient.get(url)
+      JSON.parse(RestClient.get(url))      
     end
 
     render json: response, status: :ok
