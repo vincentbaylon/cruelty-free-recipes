c37dfe2db4dd02e38b8a05d56f6594358586a4d9
VinceBaylon <vincentbaylon@gmail.com>
2022-01-04 09:10:32 -0800

    Fixed recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,7 +8,7 @@ class RecipesController < ApplicationController
       id = ENV["APP_ID"]
       key = ENV["APP_KEY"]
       url = "https://api.edamam.com/api/recipes/v2?type=public&q=vegan&app_id=#{id}&app_key=#{key}&health=vegan"
-      response = RestClient.get(url)
+      RestClient.get(url)
     end
 
     render json: response, status: :ok
