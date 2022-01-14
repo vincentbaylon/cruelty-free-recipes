9d972598287722773006a09698b5fdd1cd6e187e
VinceBaylon <vincentbaylon@gmail.com>
2022-01-14 10:28:09 -0800

    Showing rank on recipe card

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,3 +1,7 @@
+import { useState, useEffect } from 'react'
+import DisplayLeaf from './DisplayLeaf'
+import { RiTimer2Line } from 'react-icons/ri'
+
 function RecipeCard({
 	recipes,
 	setSelectedRecipe,
@@ -6,6 +10,18 @@ function RecipeCard({
 	selectedRank,
 	setSelectedRank,
 }) {
+	const [currentRating, setCurrentRating] = useState(0)
+
+	useEffect(() => {
+		let sum = 0
+		recipeRank.map((r) => {
+			sum = sum + r.rank
+		})
+		sum = sum / recipeRank.length
+
+		setCurrentRating(Math.round(sum))
+	}, [recipeRank])
+
 	const recipeClick = () => {
 		setSelectedRecipe(recipes)
 		setSelectedRank(recipeRank)
@@ -22,15 +38,25 @@ function RecipeCard({
 				{recipes.title}
 			</h1>
 			<p className='text-sm'>{recipes.sourceName}</p>
-			<h1 className='text-sm font-semibold'>
-				Prep time:{' '}
-				{recipes.readyInMinutes === 0
-					? 'N/A'
-					: recipes.readyInMinutes + ' mins'}
-			</h1>
+
 			<h1 className='text-sm font-semibold text-amber-800'>
 				{recipes.glutenFree ? 'Gluten-Free' : null}
 			</h1>
+			<div className='flex flex-row justify-between items-center'>
+				<div className='flex flex-row items-center'>
+					<RiTimer2Line />
+					<h1 className='pl-1 text-sm font-semibold'>
+						{' '}
+						{recipes.readyInMinutes === 0
+							? 'N/A'
+							: recipes.readyInMinutes + ' mins'}
+					</h1>
+				</div>
+
+				<div className='flex flex-row'>
+					{currentRating > 0 ? <DisplayLeaf rank={currentRating} /> : null}
+				</div>
+			</div>
 		</div>
 	)
 }
